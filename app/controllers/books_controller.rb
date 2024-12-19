f73da72477ef8ea534d332c115f4c2db6aeba688
joshualamai <joshualamai2004@gmail.comgit config --global user.email joshualamai2004@gmail.com>
2024-12-19 13:40:06 +0000

    inital commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: "Book was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to books_path, notice: "Book was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: "Book was successfully deleted."
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :published_date, :description)
+  end
+end
+
+  def destroy
+    @book.destroy
+    flash[:notice] = "Book was successfully deleted."
+    redirect_to books_path
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :published_date, :description)
+  end
